5788d78cd27481e8e8ca53b08e165060999894e9
Hikmet <ka555861@gmail.com>
2024-05-02 05:35:44 +0300

    Replace a tag with Link in about-us person card.

--- src/components/profile-card.tsx
+++ src/components/profile-card.tsx
@@ -1,52 +1,55 @@
+import Link from "next/link";
 import styles from "./profile-card.module.scss";
 import { jost } from "@/fonts";
 
 interface Data {
-    name: string;
-    title: string;
-    imgsrc?: string;
-    phone?: string;
-    email?: string;
-    building?: string;
+  name: string;
+  title: string;
+  imgsrc?: string;
+  phone?: string;
+  email?: string;
+  building?: string;
 }
 
 export default function ProfileCard(props: Data) {
-    return (
-        <a className={styles["profile-card"]} href="/about-us/david">
+  return (
+    <Link className={styles["profile-card"]} href="/about-us/david">
+      <div className={styles["profile-frame"]}>
+        <img src={props.imgsrc}></img>
+      </div>
 
-            <div className={styles["profile-frame"]}>
-                <img src={props.imgsrc}></img>
+      <h3>{props.name}</h3>
+      <p className={`${jost.className} ${styles["title"]}`}>{props.title}</p>
+      <div className={styles["subset"]}>
+        {props.phone && (
+          <div>
+            <hr />
+            <div className={styles["attribute"]}>
+              <img src="/icons/phone.svg"></img>
+              <p>{props.phone}</p>
             </div>
-
-            <h3>{props.name}</h3>
-            <p className={`${jost.className} ${styles["title"]}`}>{props.title}</p>
-            <div className={styles["subset"]}>
-                {props.phone &&
-                    <div>
-                        <hr />
-                        <div className={styles["attribute"]}>
-                        <img src="/icons/phone.svg"></img>
-                        <p>{props.phone}</p>
-                        </div>
-                    </div>}
-                {props.email &&
-                    <div>
-                        <hr />
-                        <div className={styles["attribute"]}>
-                        <img src="/icons/mail.svg"></img>
-                        <p>{props.email}</p>
-                        </div>
-                    </div>}
-                {props.building &&
-                    <div>
-                        <hr />
-                        <div className={styles["attribute"]}>
-                        <img src="/icons/apartment.svg"></img>
-                        <p>{props.building}</p>
-                        </div>
-                    </div>}
-                <hr />
+          </div>
+        )}
+        {props.email && (
+          <div>
+            <hr />
+            <div className={styles["attribute"]}>
+              <img src="/icons/mail.svg"></img>
+              <p>{props.email}</p>
             </div>
-        </a>
-    )
+          </div>
+        )}
+        {props.building && (
+          <div>
+            <hr />
+            <div className={styles["attribute"]}>
+              <img src="/icons/apartment.svg"></img>
+              <p>{props.building}</p>
+            </div>
+          </div>
+        )}
+        <hr />
+      </div>
+    </Link>
+  );
 }
