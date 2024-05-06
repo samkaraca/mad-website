330f148f05fde4b7175b5eadea4ba25d752dcfa7
Nehir Dündar <60262378+Martik086@users.noreply.github.com>
2024-05-07 02:34:02 +0300

    Install projects page.

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,8 @@
-import { IProjectsSection } from "@/types/projects-page";
-import { readProjects } from "@/utils/read-file";
+import React from 'react';
+import { IProjectsSection } from '@/types/projects-page';
+import ProjectCard from '@/components/project-card';
+import { readProjects } from '@/utils/read-file';
+import styles from "./projects.module.scss";
 
 export async function getStaticProps() {
   return {
@@ -9,10 +12,19 @@ export async function getStaticProps() {
   };
 }
 
-export default function Projects({
-  projectsSection,
-}: {
-  projectsSection: IProjectsSection[];
-}) {
-  return <div></div>;
-}
+export default function Projects({ projectsSection }: { projectsSection: IProjectsSection[] }) {
+  return (
+    <div>
+      {projectsSection.map((section, index) => (
+        <div key={index}>
+          <h2 className={styles["title"]}>{section.title}</h2>
+          <div className={styles.projectsContainer}>
+          {section.projects.map((project, idx) => (
+            <ProjectCard key={idx} project={project} />
+          ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
