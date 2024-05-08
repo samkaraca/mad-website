eedba9d4130b49d3c970ef6212787fa68bbc2e3e
Hikmet <ka555861@gmail.com>
2024-05-09 02:29:58 +0300

    Add acknowledgement section to the footer.

--- src/features/footer/index.tsx
+++ src/features/footer/index.tsx
@@ -18,23 +18,29 @@ export const Footer = ({
         isOpen={hamburgerMenuStatus === "open"}
         onClick={() => setHamburgerMenuStatus("closed")}
       />
-      <section className={`${styles["contact"]}`}>
-        <h2 className={`${jost.className}`}>CONTACT INFO</h2>
-        <div className={styles["divider"]} />
-        <p className={`${crimsonPro.className}`}>
-          Massey University East Precinct <br />
-          Albany Expressway (SH17) <br />
-          Albany <br />
-          0632 <br />
-          New Zealand <br /> <br />
-          <a href="mailto:mad@massey.ac.nz">mad@massey.ac.nz</a>
-        </p>
+      <section className={styles["main-content"]}>
+        <section className={`${styles["contact"]}`}>
+          <h2 className={`${jost.className}`}>CONTACT INFO</h2>
+          <div className={styles["divider"]} />
+          <p className={`${crimsonPro.className}`}>
+            Massey University East Precinct <br />
+            Albany Expressway (SH17) <br />
+            Albany <br />
+            0632 <br />
+            New Zealand <br /> <br />
+            <a href="mailto:mad@massey.ac.nz">mad@massey.ac.nz</a>
+          </p>
+        </section>
+        <section className={styles["copyright"]}>
+          <div className={styles["divider"]} />
+          <p className={jost.className}>
+            Copyright © 1998 – 2024 Massey University. All rights reserved.
+          </p>
+        </section>
       </section>
-      <section className={styles["copyright"]}>
-        <div className={styles["divider"]} />
-        <p className={jost.className}>
-          Copyright © 1998 – 2024 Massey University. All rights reserved.
-        </p>
+      <section className={styles["acknowledgement"]}>
+        This website was built by Hikmet Sami Karaca and Ali Nehir Dündar with
+        🧡
       </section>
     </footer>
   );
