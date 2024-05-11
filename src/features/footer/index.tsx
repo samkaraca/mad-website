09f6428bd6e977ed4e393e1e87fb04e81b15c60d
Hikmet <ka555861@gmail.com>
2024-05-11 04:58:31 +0300

    Add kiwi bird photo in the footer. Add acknowldgement section above footer on about-us page.

--- src/features/footer/index.tsx
+++ src/features/footer/index.tsx
@@ -2,6 +2,7 @@ import { crimsonPro, jost } from "@/fonts";
 import styles from "./footer.module.scss";
 import { Overlay } from "@/components/overlay";
 import { HamburgerMenuStatus } from "@/hooks/useHamburgerMenu";
+import { usePathname } from "next/navigation";
 
 interface Props {
   hamburgerMenuStatus: HamburgerMenuStatus;
@@ -12,35 +13,55 @@ export const Footer = ({
   hamburgerMenuStatus,
   setHamburgerMenuStatus,
 }: Props) => {
+  const pathName = usePathname();
+
   return (
     <footer id="footer" className={styles["footer"]}>
+      <section
+        style={{
+          padding: "0.5rem 2rem",
+          backgroundColor: "grey",
+          visibility: pathName === "/about-us" ? "visible" : "hidden",
+          position: pathName === "/about-us" ? "initial" : "absolute",
+          zIndex: 1,
+        }}
+      >
+        Acknowledgement: This website was built by Ali Nehir Dündar and Hikmet
+        Sami Karaca with 🧡
+      </section>
       <Overlay
         isOpen={hamburgerMenuStatus === "open"}
         onClick={() => setHamburgerMenuStatus("closed")}
       />
-      <section className={styles["main-content"]}>
-        <section className={`${styles["contact"]}`}>
-          <h2 className={`${jost.className}`}>CONTACT INFO</h2>
-          <div className={styles["divider"]} />
-          <p className={`${crimsonPro.className}`}>
-            Massey University East Precinct <br />
-            Albany Expressway (SH17) <br />
-            Albany <br />
-            0632 <br />
-            New Zealand <br /> <br />
-            <a href="mailto:mad@massey.ac.nz">mad@massey.ac.nz</a>
-          </p>
-        </section>
-        <section className={styles["copyright"]}>
-          <div className={styles["divider"]} />
-          <p className={jost.className}>
-            Copyright © 1998 – 2024 Massey University. All rights reserved.
-          </p>
+      <section className={styles["above-acknowledgement"]}>
+        <section className={styles["main-content"]}>
+          <section className={`${styles["contact"]}`}>
+            <h2 className={`${jost.className}`}>CONTACT INFO</h2>
+            <div className={styles["divider"]} />
+            <p className={`${crimsonPro.className}`}>
+              Massey University East Precinct <br />
+              Albany Expressway (SH17) <br />
+              Albany <br />
+              0632 <br />
+              New Zealand <br /> <br />
+              <a href="mailto:mad@massey.ac.nz">mad@massey.ac.nz</a>
+            </p>
+          </section>
+          <img src="/kiwi-bird.svg" alt="Kiwi bird" />
         </section>
       </section>
-      <section className={styles["acknowledgement"]}>
-        This website was built by Hikmet Sami Karaca and Ali Nehir Dündar with
-        🧡
+      <section className={styles["copyright"]}>
+        <p
+          className={jost.className}
+          style={{
+            borderTop: "1px solid grey",
+            color: "grey",
+            marginInline: "2rem",
+            paddingBlock: "1rem",
+          }}
+        >
+          Copyright © 1998 – 2024 Massey University. All rights reserved.
+        </p>
       </section>
     </footer>
   );
