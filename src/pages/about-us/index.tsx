cc7a10a21af3db630851cffde26b3b36b0ced81e
Hikmet <ka555861@gmail.com>
2024-05-11 05:04:29 +0300

    Simplify About Us page layout.

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -1,8 +1,8 @@
-import { DefaultMainContainer } from "@/components/default-main-container";
-import styles from "./about-us.module.scss";
 import ProfileCard from "@/components/profile-card";
 import { readPeople } from "@/utils/read-file";
 import { IPersonCard } from "@/types/person-card";
+import { DefaultBanner } from "@/components/default-banner";
+import { DefaultCardGrid } from "@/components/default-card-grid";
 
 export async function getStaticProps() {
   return {
@@ -14,25 +14,15 @@ export async function getStaticProps() {
 
 export default function AboutUs({ people }: { people: IPersonCard[] }) {
   return (
-    <DefaultMainContainer
-      title="About Us"
-      image="/about-us-bg.jpg"
-      imageAlt="A group of people talking in an office"
-    >
-      <div className={styles["content"]}>
-        <div className={styles["white-background"]}>
-          <div className={styles["readable"]}>
-            <section className={styles["welcome-text"]}>
-              <h1 className={styles["bottom-line"]}>Meet Our Team</h1>
-            </section>
-            <div className={styles["profile-display"]}>
-              {people.map((lecturer, index) => (
-                <ProfileCard key={index} {...lecturer} />
-              ))}
-            </div>
-          </div>
-        </div>
+    <main>
+      <DefaultBanner title="People" />
+      <div style={{ paddingTop: "2.5rem", paddingBottom: "10rem" }}>
+        <DefaultCardGrid>
+          {people.map((lecturer, index) => (
+            <ProfileCard key={index} {...lecturer} />
+          ))}
+        </DefaultCardGrid>
       </div>
-    </DefaultMainContainer>
+    </main>
   );
 }
